b7d651882aee954710a1f3cb4d0750760d8e6e0e
Evgenii Nikitin <mail.nikitin@mail.ru>
2019-11-02 17:21:45 +0300

    Update main.go

--- revers-string/main.go
+++ revers-string/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func reverse(s string) string{
-	r := []byte(s)
-	for i,j := 0, len(s)-1; i < j;i,j = i+1, j-1{
+	r := []rune(s)
+	for i,j := 0, len(r)-1; i < j;i,j = i+1, j-1{
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
 }
 
 func main() {
-	fmt.Println(reverse("Hello world!"))
+	fmt.Println(reverse("Привет!"))
 }
