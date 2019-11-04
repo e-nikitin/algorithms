b1e715b12c4960764573e19cf819e814915a1850
Evgenii Nikitin <mail.nikitin@mail.ru>
2019-11-04 10:17:00 +0300

    Update main.go

--- sort/insertion/main.go
+++ sort/insertion/main.go
@@ -3,19 +3,35 @@ package main
 import "fmt"
 
 func main() {
-  d := [...]int{2,0,9,5,3,1,6,8,4,7}
-	//d := [...]int{2,1}
-	//d := [...]int{0}
   
+	fmt.Println("From right to left")
+
+	d := [...]int{2,0,9,5,3,1,6,8,4,7}
 	fmt.Println(d)
-  
-	// From right to left
+
 	for i := len(d)-2; i >= 0; i-- {
 		for j := i; j < len(d)-1 && d[j] > d[j+1]; j++ {
 			d[j],d[j+1] = d[j+1], d[j]
-			fmt.Printf("SWAP:%d <-- %d\n", d[j], d[j+1])
+			fmt.Printf("\t\tSWAP:%d <-- %d\n", d[j], d[j+1])
 		}
+		fmt.Printf("\t%v\n", d)
 	}
-  
-	fmt.Println(d)
+
+	fmt.Println("--------------------------------------------------")
+
+	fmt.Println("From left to right")
+
+	a := [...]int{9,5,1,2,0,3,6,7,8}
+	fmt.Println(a)
+
+	for i:=1; i<len(a);i++  {
+		for j:=i-1; j >= 0 && a[j] > a[j+1]; j-- {
+			a[j], a[j+1] = a[j+1], a[j]
+			fmt.Printf("\t\tSWAP:%d <-- %d\n", a[j+1], a[j])
+		}
+		fmt.Printf("\t%v\n", a)
+	}
+
+
+	fmt.Println(a)
 }
