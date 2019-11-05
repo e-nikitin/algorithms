cde2da658fd3d2390d8b0334939ce25d228a167f
Evgenii Nikitin <mail.nikitin@mail.ru>
2019-11-05 04:46:14 +0300

    Create main.go

---
+++ sort/bubble/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := [...]int{9,1,8,3,7,3,6,4,5,2,0}
+  
+	fmt.Println(a)
+  
+  for {
+		b := true
+		for i:=1; i<len(a);i++{
+			if a[i] < a[i-1] {
+				a[i], a[i-1] = a[i-1], a[i]
+				b = false
+				fmt.Printf("\t\tSWAP: %d <-- %d\n", a[i], a[i-1])
+			}
+		}
+    
+		fmt.Printf("\t%v\n", a)
+    
+		if b {break}
+	}
+  
+  fmt.Println(a)
+
+}
